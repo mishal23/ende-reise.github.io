380410c5e9b67a09e8850e980b1edb3013a58fb5
ende-reise <renaldy.hrbp@gmail.com>
2024-12-25 22:57:29 +0700

    initiate commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY TO YOU YAAA, AYA!!').split('')
+    const titles = ('HAPPY BIRTHDAY TO YOU, AYA!!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
