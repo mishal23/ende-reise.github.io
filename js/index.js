aff707ec40ae3a974f510ea08e7dd1b2de3d772a
Mishal Shah <shahmishal1998@gmail.com>
2025-03-13 17:38:02 +0000

    some changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I have something for you!!'.split('')
+const text = 'Cheers to the special day! Here\'s something for you!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
